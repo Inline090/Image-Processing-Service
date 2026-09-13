33a9e77d7907c45486bd7d8fcd4528ed38aad188
inline <navneet2k18@gmail.com>
2026-09-13 15:43:22 +0530

    chore(server): add ESLint and Prettier config for TypeScript

---
+++ server/eslint.config.js
@@ -0,0 +1,13 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { ignores: ['dist/**'] },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    extends: [...tseslint.configs.recommended],
+  },
+  prettier,
+);
